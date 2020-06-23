345eb3554ea0565f8b5342ffc030e0cb851f720c
Manuela Garcia <mgh9517@gmail.com>
2020-06-23 11:27:54 -0500

    Fetch firestore data using a future builder

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:fluttershare/widgets/header.dart';
+import 'package:fluttershare/widgets/progress.dart';
 
 final usersRef = Firestore.instance.collection('users');
 
@@ -15,18 +16,8 @@ class _TimelineState extends State<Timeline> {
 
   @override
   void initState() {
-    getUsers();
     //getUserById();
     super.initState();
-  }
-
-  getUsers() async {
-    final QuerySnapshot snapshot = await usersRef.getDocuments();
-    snapshot.documents.forEach((DocumentSnapshot doc) {
-      setState(() {
-        users = snapshot.documents;
-      });
-    });
   }
 
   getUserById() async {
@@ -40,16 +31,22 @@ class _TimelineState extends State<Timeline> {
   @override
   Widget build(context) {
     return Scaffold(
-      appBar: header(context, isAppTitle: true),
-      body: Container(
-        child: ListView(
-          children: users
-              .map(
-                (user) => Text(user['username']),
-              )
-              .toList(),
-        ),
-      ),
-    );
+        appBar: header(context, isAppTitle: true),
+        body: FutureBuilder<QuerySnapshot>(
+          future: usersRef.getDocuments(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return circularProgress();
+            }
+            final List<Text> children = snapshot.data.documents
+                .map((doc) => Text(doc['username']))
+                .toList();
+            return Container(
+              child: ListView(
+                children: children,
+              ),
+            );
+          },
+        ));
   }
 }
