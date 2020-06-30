9c0ff056e5d26ab7d9807497ed0d07aadf25fabc
Manuela Garcia <mgh9517@gmail.com>
2020-06-29 20:36:19 -0500

    Change rules so user could access the camera and gallery of the phone

--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Upload extends StatefulWidget {
   @override
@@ -6,8 +9,88 @@ class Upload extends StatefulWidget {
 }
 
 class _UploadState extends State<Upload> {
+  File file;
+  final picker = ImagePicker();
+
+  handleTakePhoto() async {
+    Navigator.pop(context);
+    PickedFile pickedFile = await picker.getImage(
+        source: ImageSource.camera, maxHeight: 675, maxWidth: 960);
+    setState(() {
+      file = File(pickedFile.path);
+    });
+  }
+
+  handleChooseFromGallery() async {
+    Navigator.pop(context);
+    PickedFile pickedFile = await picker.getImage(source: ImageSource.gallery);
+    setState(() {
+      file = File(pickedFile.path);
+    });
+  }
+
+  selectImage(parentContext) async {
+    return await showDialog(
+        context: parentContext,
+        builder: (context) {
+          return SimpleDialog(
+            title: Text('Create post'),
+            children: <Widget>[
+              SimpleDialogOption(
+                child: Text('Photo with camera'),
+                onPressed: handleTakePhoto,
+              ),
+              SimpleDialogOption(
+                child: Text('Image from gallery'),
+                onPressed: handleChooseFromGallery,
+              ),
+              SimpleDialogOption(
+                child: Text('Cancel'),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          );
+        });
+  }
+
+  Container buildSplashScreen() {
+    return Container(
+      color: Theme.of(context).accentColor.withOpacity(0.6),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SvgPicture.asset(
+            'assets/images/upload.svg',
+            height: 260,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20.0),
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Text(
+                'Upload Image',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                ),
+              ),
+              color: Colors.deepOrange,
+              onPressed: () => selectImage(context),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  buildUploadForm() {
+    return Text('File loaded');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Upload");
+    return file == null ? buildSplashScreen() : buildUploadForm();
   }
 }
