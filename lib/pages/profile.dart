8236e2185db325484a3683194b87d34d1ff9529e
Manuela Garcia <mgh9517@gmail.com>
2020-07-01 13:51:21 -0500

    Start the profile page design

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,17 +1,139 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttershare/pages/home.dart';
 import 'package:fluttershare/widgets/header.dart';
+import 'package:fluttershare/widgets/progress.dart';
+
+import 'package:fluttershare/models/user.dart';
 
 class Profile extends StatefulWidget {
+  String profileId;
+
+  Profile({this.profileId});
+
   @override
   _ProfileState createState() => _ProfileState();
 }
 
 class _ProfileState extends State<Profile> {
+  Column buildCountColumn(String label, int count) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Text(
+          count.toString(),
+          style: TextStyle(
+            fontSize: 22.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 4.0),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 15.0,
+              color: Colors.grey,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Text buildProfileButton() {
+    return Text('Profile button');
+  }
+
+  buildProfileHeader() {
+    return FutureBuilder(
+      future: usersRef.document(widget.profileId).get(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return circularProgress();
+        }
+        User user = User.fromDocument(snapshot.data);
+        return Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 40.0,
+                    backgroundColor: Colors.grey,
+                    backgroundImage: CachedNetworkImageProvider(user.photoUrl),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            buildCountColumn('posts', 0),
+                            buildCountColumn('followers', 0),
+                            buildCountColumn('following', 0),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            buildProfileButton(),
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(top: 12.0),
+                child: Text(
+                  user.username,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(top: 4.0),
+                child: Text(
+                  user.displayName,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(top: 2.0),
+                child: Text(
+                  user.bio,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: header(context, titleText: 'Profile'),
-      body: Text('Profile'),
+      body: ListView(
+        children: <Widget>[
+          buildProfileHeader(),
+        ],
+      ),
     );
   }
 }
